7613436a946286f54d33cb86a7d2a8f3269520fd
sagar dhone <sdhone98@gmail.com>
2025-05-28 00:22:03 +0530

    media file path added

--- automate_accounts/settings.py
+++ automate_accounts/settings.py
@@ -34,6 +34,10 @@ INSTALLED_APPS = [
     'receipts',
 ]
 
+# MEDIA FILE SETTINGS
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
