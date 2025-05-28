72606a49918868fa529fbb9649007723a41d684c
sagar dhone <sdhone98@gmail.com>
2025-05-29 01:11:26 +0530

    Receipt model attribute name updated

--- receipts/models.py
+++ receipts/models.py
@@ -27,7 +27,7 @@ class Receipt(models.Model):
     purchased_at = models.DateTimeField(null=True, blank=True)
     merchant_name = models.CharField(max_length=255, null=True, blank=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    file_path = models.ForeignKey(ReceiptFile, on_delete=models.CASCADE)
+    receipt_file = models.ForeignKey(ReceiptFile, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
