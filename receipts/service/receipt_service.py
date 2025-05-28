f4c25aec3c557bc3d87701f6b632c9bf4c9a2429
sagar dhone <sdhone98@gmail.com>
2025-05-29 01:12:50 +0530

    query para filters added for api

--- receipts/service/receipt_service.py
+++ receipts/service/receipt_service.py
@@ -1,10 +1,17 @@
 from receipts.models import Receipt
 
 
-def fetch_all_process_receipts():
-    return Receipt.objects.all()
+def fetch_all_process_receipts(filter_para):
+    q_filter = {}
+    if filter_para:
+        query_keys = filter_para.keys()
+        model_fields = {field.name for field in Receipt._meta.get_fields()}
+        valid_filter_keys = set(query_keys).intersection(model_fields)
+        q_filter = {key: filter_para[key] for key in valid_filter_keys}
+    result = Receipt.objects.filter(**q_filter).all()
+    return result
 
-def fetch_process_receipt_by_id(receipt_id):
+def fetch_process_receipt_by_id(receipt_id, filter_para):
     if not receipt_id:
         raise ValueError("receipt_id cannot be None")
 
